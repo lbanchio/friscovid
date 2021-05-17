b8f5f1c49db3ce81dce2a1f314ca56768deb008a
lbanchio <lbanchio@gmail.com>
2021-05-17 11:58:33 -0300

    once upon a time...

---
+++ domain/IRawData.ts
@@ -0,0 +1,10 @@
+export default interface IRawData { 
+    date: string,
+    new_cases: number, 
+    deaths: number,
+    hospitalized: number
+    ventilators: number,
+    recovered: number,
+    isolated: number,
+    active: number,
+ } 
