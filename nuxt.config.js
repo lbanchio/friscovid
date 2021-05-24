618e17369c9a71a447ba74b931434bc579a080ee
lbanchio <lbanchio@gmail.com>
2021-05-24 12:37:08 -0300

    Add deploy script

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/friscovid/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
